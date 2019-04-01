6216d27b990d6b6f916ff8c3afc09c11efde2858
Cristyn Magnus <cmagnus@gmail.com>
2019-04-01 14:26:23 -0400

    Animate ball

--- main.py
+++ main.py
@@ -15,22 +15,16 @@ red = (255, 0, 0)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Test")
-
+ballPosition = (0,0)
 ballRadius = 50
-ball = pygame.draw.circle(screen, red, (0, 0), ballRadius)
-ballSurface = pygame.Surface((ballRadius*2, ballRadius*2))
-ballSurface.set_alpha(0)
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    #ballrect = ballrect.move(speed)
-    '''if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]'''
-
     screen.fill(backgroundColor)
-    screen.blit(ballSurface, ball)
+    ballPosition = (ballPosition[0]+speed[0], ballPosition[1]+speed[1])
+
+    pygame.draw.circle(screen, red, ballPosition, ballRadius)
+    
     pygame.display.flip()
