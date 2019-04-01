f5497b7c1ece3294187178bad039ffa1b9f12df8
Cristyn Magnus <cmagnus@gmail.com>
2019-04-01 15:25:22 -0400

    Make Circle defaults random

--- Circle.py
+++ Circle.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 
 class Circle:
-  def __init__(self, window, color, position, radius, speed):
+  def __init__(self, window, color = (random.randrange(0,256),random.randrange(0,256),random.randrange(0,256)), position = (random.randrange(0, 320),random.randrange(0, 240)), radius=random.randrange(1, 100), speed=[0,0]):
     self.window = window
     self.color = color
     self.position = position
