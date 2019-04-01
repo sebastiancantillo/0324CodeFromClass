df8055b3898bb737a63464ca1ebca46f6efd765f
Cristyn Magnus <cmagnus@gmail.com>
2019-04-01 15:58:09 -0400

    Add boundary conditional

--- Circle.py
+++ Circle.py
@@ -3,6 +3,7 @@ import random
 
 class Circle:
   def __init__(self, window, color = (random.randrange(0,256),random.randrange(0,256),random.randrange(0,256)), position = (random.randrange(0, 320),random.randrange(0, 240)), radius=random.randrange(1, 100), speed=[0,0]):
+    #todo: boundaries. Properties to stay on the screen
     self.window = window
     self.color = color
     self.position = position
@@ -14,4 +15,19 @@ class Circle:
 
   def move(self):
     self.position = (self.position[0]+self.speed[0], self.position[1]+self.speed[1])
+
+    if (self.position[0] < 0):
+      print("left")
+    if (self.position[0] > self.windowSize[0]):
+      print("right")
+    if (self.position[1] < 0):
+      print("top")
+    if (self.position[1] > self.windowSize[1]):
+      print("bottom")
+
+# Dictionary Format: {"top": "sticky", "bottom": "scroll", "left": "bounce", "right": "gone"}
+  def setBoundaries(self, boundaryDictionary, windowSize):
+    self.boundaries = boundaryDictionary
+    self.windowSize = windowSize
+
     
--- main.py
+++ main.py
@@ -10,23 +10,17 @@ height = 240
 size = (width, height)
 '''
 
-speed = [2, 2]
+speed = [-2, -2]
 backgroundColor = (255, 255, 255)
 red = (255, 0, 0)
 
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption("Test")
-ballPosition = (50,50)
+ballPosition = (width,height)
 ballRadius = 50
 
 myCircle = Circle(screen, red, ballPosition, ballRadius, speed)
-circleBlue = Circle(screen, (0,0,255))
-circleCorner = Circle(screen, position=(320, 240))
-
-defaultCircle = Circle(screen)
-randomCircles = []
-for i in range(10):
-  randomCircles.append(Circle(screen))
+myCircle.setBoundaries({"top": "sticky", "bottom": "scroll", "left": "bounce", "right": "gone"}, (width,height))
 
 while 1:
     for event in pygame.event.get():
@@ -38,11 +32,6 @@ while 1:
     #pygame.draw.circle(screen, red, ballPosition, ballRadius)
 
     myCircle.draw()
-    defaultCircle.draw()
-    for item in randomCircles:
-      item.draw()
-    circleBlue.draw()
     myCircle.move()
-    circleCorner.draw()
     
     pygame.display.flip()
